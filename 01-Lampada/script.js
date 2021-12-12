16bc14e27bc5a7cac371e87f0a39490190098c07
Lu Silva <lusilva.91@protonmail.com>
2021-12-12 11:33:19 -0300

    Desafio Lampada

---
+++ 01-Lampada/script.js
@@ -0,0 +1,63 @@
+  const turnOn = document.getElementById('btnOnOff');
+  const lamp = document.getElementById('lamp'); 
+  const reload = document.getElementById('reload');
+
+  const isLampBroken = () => {
+    return lamp.src.indexOf('quebrada') > -1 // true
+  }
+
+  const isLampOff = () => {
+    return lamp.src.indexOf('desligada') > -1;
+  }
+
+  const lampOn = () => {
+    if(!isLampBroken()){
+      lamp.src = './img/ligada.jpg';
+      changeTitle();
+      turnOn.textContent = `Desligar`;
+    }
+  }
+
+  const lampOff = () => {
+    if(!isLampBroken()){
+      lamp.src = './img/desligada.jpg';
+      changeTitle();
+      turnOn.textContent = `Ligar`;
+    }
+  }
+
+  const lampBroken = () => { 
+    lamp.src = './img/quebrada.jpg';
+    displayMessage();
+  }
+
+  const reloadPage = () => {
+    location.reload();
+  }
+
+  const clickButtonLamp = () => {
+    if(isLampOff()){
+      turnOn.textContent = `Desligar`;
+      lampOn();
+    } else {
+      lampOff();
+      turnOn.textContent = `Ligar`;
+    }
+  }
+
+  const displayMessage = () => {
+    let message = document.getElementById('message');
+    message.style.display = 'block';
+    message.textContent = `Oh no! A lâmpada quebrou! \nRecarregue a página.`;
+  }
+
+  const changeTitle = () => {
+    let title = document.getElementById('mainTitle')
+    isLampOff() !== true ? title.textContent = `Apague a Lâmpada` :  title.textContent = `Acenda a Lâmpada`;
+  }
+  
+  turnOn.addEventListener('click', clickButtonLamp);
+  lamp.addEventListener('mouseover', lampOn);
+  lamp.addEventListener('mouseleave', lampOff);
+  lamp.addEventListener('dblclick', lampBroken);
+  reload.addEventListener('click', reloadPage);
